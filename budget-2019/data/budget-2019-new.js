3322e25fe383460817373beac14a5e34185c527a
David Klionsky <davidklionsky@gmail.com>
2019-03-31 08:40:36 -0700

    Add spending for Corps of Engineers, DOI

--- budget-2019/data/budget-2019-new.js
+++ budget-2019/data/budget-2019-new.js
@@ -1,77 +1,183 @@
 var budget = {
-  "id": "united-states-federal-budget%2C-2019",
-  "name": "United States Federal Budget, 2019",
-  "children": [
-    {
-      "id": "mandatory-spending",
-      "name": "Mandatory Spending",
-      "description": "Mandatory programs are programs that are guaranteed to receive a certain amount of funding every year by law. Unlike discretionary programs, which have a budget set every year by congress, mandatory programs can only have their spending changed by amending the original law. Social Security and Medicare are the largest sources of mandatory spending.",
-      "children": [
-        {
-          "id": "social-security",
-          "name": "Social Security",
-          "description": "Social Security is a program that gives money to people who are retired, disabled, or living in poverty. In 2019, the average retiree will receive $1,461 per month from Social Security. The program is funded by a tax on your paycheck, with 6.2% paid by you and 6.2% paid by your employer. There's an ongoing debate about how we will continue to fund Social Security. Today's workers pay for today's retirees, and the number of people receiving benefits is increasing relative to the number of workers who contribute through taxes. For example, the average number of workers per retiree was 5.1 in 1960. This declined to 3.0 by 2010, and is projected to decline to 2.2 by 2030. If nothing changes, it's predicted that Social Security will start paying out more than it takes in by 2033. Congress might prevent this shortfall by increasing taxes, decreasing benefits, or a combination of both.",
-          "amount": 1046000000000.0
-        },
-        {
-          "id": "medicare",
-          "name": "Medicare",
-          "description": "Medicare is a government health insurance program for people aged 65 and older and for some people with disabilities. Like Social Security, Medicare is funded through a tax on your paycheck, with you and your employer each paying 1.45%. Also like Social Security, spending on Medicare is expected to increase, as the number of eligible people over 65 increases and the cost of health care in the United States rises. Congress could avoid this shortfall by increasing payroll taxes, reducing benefits, or by finding ways to reduce health care and prescription drug costs in general.",
-          "amount": 625000000000.0
-        },
-        {
-          "id": "medicaid",
-          "name": "Medicaid",
-          "description": "Medicaid is a government health insurance program for people of any age who are living near or below the poverty line. It is jointly funded by the federal government and the states; states don't have to participate, but if they do, the federal government matches a portion of the state funding. ",
-          "amount": 412000000000.0
-        },
-        {
-          "id": "exchange-subsidies",
-          "name": "Exchange subsidies",
-          "description": "The Affordable Care Act, also known as Obamacare, expanded health care coverage to 24 million people, reducing the number of uninsured people in the United States by almost half. One major way this was done was by subsidizing health insurance policies purchased through an online marketplace (\"exchange subsidies\"). These subsidies are mostly paid for through taxes on health insurance providers, prescription drug manufacturers, and investment income for high income individuals (mostly in the top 1% of earners).",
-          "amount": 45000000000.0
-        },
-        {
-          "id": "income-security-programs",
-          "name": "Income Security Programs",
-          "description": "Income security programs include tax credits for low income families, cash assistance, and unemployment benefits.",
-          "amount": 221130000000.0
-        },
-        {
-          "id": "federal-civilian-and-military-retirement",
-          "name": "Federal Civilian and Military Retirement",
-          "description": "Civilians who work for the federal government as well as members of the armed forces qualify for retirement benefits after serving for a certain number of years, similar to a pension.",
-          "amount": 119070000000.0
-        },
-        {
-          "id": "agriculture-and-food",
-          "name": "Agriculture and Food",
-          "description": "Most spending in this category is for meal programs like the Supplemental Nutrition Assistance Program (SNAP), also known as food stamps, and school lunches for children from low-income families. It also includes subsidies to farmers who produce certain crops.",
-          "amount": 102060000000.0
-        },
-        {
-          "id": "veterans%E2%80%99-benefits",
-          "name": "Veterans\u2019 Benefits",
-          "description": "Some veterans of the armed forces qualify for additional benefits, such as special health insurance programs.",
-          "amount": 68040000000.00001
-        },
-        {
-          "id": "other-mandatory-programs",
-          "name": "Other mandatory programs",
-          "amount": 56700000000.0
-        }
-      ]
-    },
-    {
-      "id": "interest-on-the-national-debt",
-      "name": "Interest on the National Debt",
-      "description": "Sometimes the federal government wants to spend more money than it takes in through taxes. Some of the extra money comes from people, companies, or other governments lending money to the federal government (e.g. through treasury bonds). The government has to pay back interest on that debt. As of December 31, 2018, the total national debt was $21.97 trillion.",
-      "amount": 363000000000.0,
-    },
-    {
-      "id": "discretionary-spending",
-      "name": "Discretionary Spending",
-      "description": "Explain what discretionary spending is here.",
-    },
-  ],
+	"id": "united-states-federal-budget-2019",
+	"name": "United States Federal Budget, 2019",
+	"children": [
+		{
+			"id": "mandatory-spending",
+			"name": "Mandatory Spending",
+			"description": "Mandatory programs are programs that are guaranteed to receive a certain amount of funding every year by law. Unlike discretionary programs, which have a budget set every year by congress, mandatory programs can only have their spending changed by amending the original law. Social Security and Medicare are the largest sources of mandatory spending.",
+			"children": [
+				{
+					"id": "social-security",
+					"name": "Social Security",
+					"description": "Social Security is a program that gives money to people who are retired, disabled, or living in poverty. In 2019, the average retiree will receive $1,461 per month from Social Security. The program is funded by a tax on your paycheck, with 6.2% paid by you and 6.2% paid by your employer. There's an ongoing debate about how we will continue to fund Social Security. Today's workers pay for today's retirees, and the number of people receiving benefits is increasing relative to the number of workers who contribute through taxes. For example, the average number of workers per retiree was 5.1 in 1960. This declined to 3.0 by 2010, and is projected to decline to 2.2 by 2030. If nothing changes, it's predicted that Social Security will start paying out more than it takes in by 2033. Congress might prevent this shortfall by increasing taxes, decreasing benefits, or a combination of both.",
+					"amount": 1046000000000.0
+				},
+				{
+					"id": "medicare",
+					"name": "Medicare",
+					"description": "Medicare is a government health insurance program for people aged 65 and older and for some people with disabilities. Like Social Security, Medicare is funded through a tax on your paycheck, with you and your employer each paying 1.45%. Also like Social Security, spending on Medicare is expected to increase, as the number of eligible people over 65 increases and the cost of health care in the United States rises. Congress could avoid this shortfall by increasing payroll taxes, reducing benefits, or by finding ways to reduce health care and prescription drug costs in general.",
+					"amount": 625000000000.0
+				},
+				{
+					"id": "medicaid",
+					"name": "Medicaid",
+					"description": "Medicaid is a government health insurance program for people of any age who are living near or below the poverty line. It is jointly funded by the federal government and the states; states don't have to participate, but if they do, the federal government matches a portion of the state funding. ",
+					"amount": 412000000000.0
+				},
+				{
+					"id": "exchange-subsidies",
+					"name": "Exchange subsidies",
+					"description": "The Affordable Care Act, also known as Obamacare, expanded health care coverage to 24 million people, reducing the number of uninsured people in the United States by almost half. One major way this was done was by subsidizing health insurance policies purchased through an online marketplace (\"exchange subsidies\"). These subsidies are mostly paid for through taxes on health insurance providers, prescription drug manufacturers, and investment income for high income individuals (mostly in the top 1% of earners).",
+					"amount": 45000000000.0
+				},
+				{
+					"id": "income-security-programs",
+					"name": "Income Security Programs",
+					"description": "Income security programs include tax credits for low income families, cash assistance, and unemployment benefits.",
+					"amount": 221130000000.0
+				},
+				{
+					"id": "federal-civilian-and-military-retirement",
+					"name": "Federal Civilian and Military Retirement",
+					"description": "Civilians who work for the federal government as well as members of the armed forces qualify for retirement benefits after serving for a certain number of years, similar to a pension.",
+					"amount": 119070000000.0
+				},
+				{
+					"id": "agriculture-and-food",
+					"name": "Agriculture and Food",
+					"description": "Most spending in this category is for meal programs like the Supplemental Nutrition Assistance Program (SNAP), also known as food stamps, and school lunches for children from low-income families. It also includes subsidies to farmers who produce certain crops.",
+					"amount": 102060000000.0
+				},
+				{
+					"id": "veterans-benefits",
+					"name": "Veterans\u2019 Benefits",
+					"description": "Some veterans of the armed forces qualify for additional benefits, such as special health insurance programs.",
+					"amount": 68040000000.00001
+				},
+				{
+					"id": "other-mandatory-programs",
+					"name": "Other mandatory programs",
+					"amount": 56700000000.0
+				}
+			]
+		},
+		{
+			"id": "interest-on-the-national-debt",
+			"name": "Interest on the National Debt",
+			"description": "Sometimes the federal government wants to spend more money than it takes in through taxes. Some of the extra money comes from people, companies, or other governments lending money to the federal government (e.g. through treasury bonds). The government has to pay back interest on that debt. As of December 31, 2018, the total national debt was $21.97 trillion.",
+			"amount": 363000000000.0,
+		},
+		{
+			"id": "discretionary-spending",
+			"name": "Discretionary Spending",
+			"description": "Explain what discretionary spending is here.",
+			"children": [
+				{
+					"id": "army-corps-of-engineers",
+					"name": "Army Corps Of Engineers",
+					"description": "The Army Corps of Engineers is an agency under the Department of Defense. Its mission is to deliver vital public and military engineering services, most notably dams, canals, and flood control systems. They also design and construct the nation's military facilities.",
+					"children": [
+						{
+							"id": "investigations",
+							"name": "Investigations",
+							"description": "For expenses necessary where authorized by law for the collection and study of basic information pertaining to river and harbor, flood and storm damage reduction, shore protection, aquatic ecosystem restoration, and related needs; for surveys and detailed studies, and plans and specifications of proposed river and harbor, flood and storm damage reduction, shore protection, and aquatic ecosystem restoration projects, and related efforts prior to construction; for restudy of authorized projects; and for miscellaneous investigations, and, when authorized by law, surveys and detailed studies, and plans and specifications of projects prior to construction.",
+							"amount": 125000000.0
+						},
+						{
+							"id": "construction",
+							"name": "Construction",
+							"description": "For expenses necessary for the construction of river and harbor, flood and storm damage reduction, shore protection, aquatic ecosystem restoration, and related projects authorized by law; for conducting detailed studies, and plans and specifications, of such projects (including those involving participation by States, local governments, or private groups) authorized or made eligible for selection by law (but such detailed studies, and plans and specifications, shall not constitute a commitment of the Government to construction).",
+							"amount": 2183000000.0
+						},
+						{
+							"id": "mississippi-river-and-tributaries",
+							"name": "Mississippi River And Tributaries",
+							"description": "For expenses necessary for flood damage reduction projects and related efforts in the Mississippi River alluvial valley below Cape Girardeau, Missouri, as authorized by law.",
+							"amount": 368000000.0
+						},
+						{
+							"id": "operation-and-maintenance",
+							"name": "Operation And Maintenance",
+							"description": "For expenses necessary for the operation, maintenance, and care of existing river and harbor, flood and storm damage reduction, aquatic ecosystem restoration, and related projects authorized by law; providing security for infrastructure owned or operated by the Corps, including administrative buildings and laboratories; maintaining harbor channels provided by a State, municipality, or other public agency that serve essential navigation needs of general commerce, where authorized by law; surveying and charting northern and northwestern lakes and connecting waters; clearing and straightening channels; and removing obstructions to navigation.",
+							"amount": 3739500000.0
+						},
+						{
+							"id": "regulatory-program",
+							"name": "Regulatory Program",
+							"description": "For expenses necessary for administration of laws pertaining to regulation of navigable waters and wetlands.",
+							"amount": 200000000.0
+						},
+						{
+							"id": "formerly-utilized-sites-remedial-action-program",
+							"name": "Formerly Utilized Sites Remedial Action Program",
+							"description": "For expenses necessary to clean up contamination from sites in the United States resulting from work performed as part of the Nation's early atomic energy program.",
+							"amount": 150000000.0
+						},
+						{
+							"id": "flood-control-and-coastal-emergencies",
+							"name": "Flood Control And Coastal Emergencies",
+							"description": "For expenses necessary to prepare for flood, hurricane, and other natural disasters and support emergency operations, repairs, and other activities in response to such disasters as authorized by law.",
+							"amount": 35000000.0
+						},
+						{
+							"id": "expenses",
+							"name": "Expenses",
+							"description": "For expenses necessary for the supervision and general administration of the civil works program in the headquarters of the Corps of Engineers and the offices of the Division Engineers; and for costs of management and operation of the Humphreys Engineer Center Support Activity, the Institute for Water Resources, the United States Army Engineer Research and Development Center, and the United States Army Corps of Engineers Finance Center allocable to the civil works program.",
+							"amount": 193000000.0
+						},
+						{
+							"id": "office-of-the-assistant-secretary-of-the-army-for-civil-works",
+							"name": "Office Of The Assistant Secretary Of The Army For Civil Works",
+							"description": "For the Office of the Assistant Secretary of the Army for Civil Works as authorized by 10 U.S.C. 3016(b)(3).",
+							"amount": 5000000.0
+						}
+					]
+				},
+				{
+					"id": "department-of-the-interior",
+					"name": "Department Of The Interior",
+					"description": "The Department of the Interior is responsible for the management and conservation of federal lands and natural resources, as well as programs relating to Native Americans. It also runs the National Park Service and wildlife refuges.",
+					"children": [
+						{
+							"id": "central-utah-project",
+							"name": "Central Utah Project",
+							"description": "For carrying out activities authorized by the Central Utah Project Completion Act.",
+							"amount": 15000000.0
+						},
+						{
+							"id": "bureau-of-reclamation",
+							"name": "Bureau Of Reclamation",
+							"children": [
+								{
+									"id": "water-and-related-resources",
+									"name": "Water And Related Resources",
+									"description": "For management, development, and restoration of water and related natural resources and for related activities, including the operation, maintenance, and rehabilitation of reclamation and other facilities, participation in fulfilling related Federal responsibilities to Native Americans, and related grants to, and cooperative and other agreements with, State and local governments, federally recognized Indian tribes, and others.",
+									"amount": 1391992000.0
+								},
+								{
+									"id": "central-valley-project-restoration-fund",
+									"name": "Central Valley Project Restoration Fund",
+									"description": "For carrying out the programs, projects, plans, habitat restoration, improvement, and acquisition provisions of the Central Valley Project Improvement Act.",
+									"amount": 62008000.0
+								},
+								{
+									"id": "california-bay-delta-restoration",
+									"name": "California Bay-Delta Restoration",
+									"description": "For carrying out activities authorized by the Water Supply, Reliability, and Environmental Improvement Act, consistent with plans to be approved by the Secretary of the Interior.",
+									"amount": 35000000.0
+								},
+								{
+									"id": "policy-and-administration",
+									"name": "Policy And Administration",
+									"description": "For expenses necessary for policy, administration, and related functions in the Office of the Commissioner, the Denver office, and offices in the five regions of the Bureau of Reclamation, to remain available until September 30, 2020.",
+									"amount": 61000000.0
+								}
+							]
+						}
+					]
+				}
+			],
+		},
+	],
 };
