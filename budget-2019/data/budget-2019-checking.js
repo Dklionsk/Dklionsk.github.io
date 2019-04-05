f8c07b9903250fe3189324b3aa1991ee252375b7
David Klionsky <davidklionsky@gmail.com>
2019-04-05 16:34:18 -0700

    Switch to new method for displaying the budget

--- budget-2019/data/budget-2019-checking.js
+++
@@ -1,7 +0,0 @@
-var budget = {
-    "id": "united-states-federal-budget-2019",
-	"name": "United States Federal Budget, 2019",
-	"children": [
-        
-    ],
-};
---
+++ budget-2019/data/budget-2019-v2.js
@@ -0,0 +1,146 @@
+var budget = {
+    "id": "united-states-federal-budget%2C-2019",
+    "name": "United States Federal Budget, 2019",
+    "children": [
+        {
+            "id": "department-of-labor",
+            "name": "Department of Labor",
+            "description": "The Department of Labor is responsible for occupational safety, wage and hour standards, unemployment insurance benefits, reemployment services, and some economic statistics. It promotes the wellbeing of America's wage earners, job seekers, and retirees.",
+            "children": [
+                {
+                    "id": "employment-and-training-administration",
+                    "name": "Employment and Training Administration",
+                    "children": [
+                        {
+                            "id": "training-and-employment-services",
+                            "name": "Training and Employment Services",
+                            "amount": 3502700000.0
+                        },
+                        {
+                            "id": "job-corps",
+                            "name": "Job Corps",
+                            "amount": 1718655000.0
+                        },
+                        {
+                            "id": "community-service-employment-for-older-americans",
+                            "name": "Community Service Employment for Older Americans",
+                            "amount": 400000000.0
+                        },
+                        {
+                            "id": "federal-unemployment-benefits-and-allowances",
+                            "name": "Federal Unemployment Benefits and Allowances",
+                            "amount": 790000000.0
+                        },
+                        {
+                            "id": "state-unemployment-insurance-and-employment-service-operations",
+                            "name": "State Unemployment Insurance and Employment Service Operations",
+                            "amount": 3335649000.0
+                        },
+                        {
+                            "id": "program-administration",
+                            "name": "Program Administration",
+                            "amount": 158656000.0
+                        }
+                    ]
+                },
+                {
+                    "id": "employee-benefits-security-administration",
+                    "name": "Employee Benefits Security Administration",
+                    "amount": 181000000.0
+                },
+                {
+                    "id": "wage-and-hour-division",
+                    "name": "Wage and Hour Division",
+                    "amount": 229000000.0
+                },
+                {
+                    "id": "office-of-labor-management-standards",
+                    "name": "Office of Labor-Management Standards",
+                    "amount": 41187000.0
+                },
+                {
+                    "id": "office-of-federal-contract-compliance-programs",
+                    "name": "Office of Federal Contract Compliance Programs",
+                    "amount": 103476000.0
+                },
+                {
+                    "id": "office-of-workers%27-compensation-programs",
+                    "name": "Office of Workers' Compensation Programs",
+                    "children": [
+                        {
+                            "id": "salaries-and-expenses",
+                            "name": "Salaries and Expenses",
+                            "amount": 117601000.0
+                        },
+                        {
+                            "id": "special-benefits",
+                            "name": "Special Benefits",
+                            "amount": 230000000.0
+                        },
+                        {
+                            "id": "special-benefits-for-disabled-coal-miners",
+                            "name": "Special Benefits for Disabled Coal Miners",
+                            "amount": 24319000.0
+                        },
+                        {
+                            "id": "administrative-expenses%2C-energy-employees-occupational-illness-compensation-fund",
+                            "name": "Administrative Expenses, Energy Employees Occupational Illness Compensation Fund",
+                            "amount": 59098000.0
+                        },
+                        {
+                            "id": "black-lung-disability-trust-fund",
+                            "name": "Black Lung Disability Trust Fund",
+                            "amount": 70240000.0
+                        }
+                    ]
+                },
+                {
+                    "id": "occupational-safety-and-health-administration",
+                    "name": "Occupational Safety and Health Administration",
+                    "amount": 557787000.0
+                },
+                {
+                    "id": "mine-safety-and-health-administration",
+                    "name": "Mine Safety and Health Administration",
+                    "amount": 373816000.0
+                },
+                {
+                    "id": "bureau-of-labor-statistics",
+                    "name": "Bureau of Labor Statistics",
+                    "amount": 615000000.0
+                },
+                {
+                    "id": "office-of-disability-employment-policy",
+                    "name": "Office of Disability Employment Policy",
+                    "amount": 38203000.0
+                },
+                {
+                    "id": "departmental-management",
+                    "name": "Departmental Management",
+                    "children": [
+                        {
+                            "id": "salaries-and-expenses-2",
+                            "name": "Salaries and Expenses",
+                            "amount": 337756000.0
+                        },
+                        {
+                            "id": "veterans-employment-and-training",
+                            "name": "Veterans Employment and Training",
+                            "amount": 300041000.0
+                        },
+                        {
+                            "id": "it-modernization",
+                            "name": "It Modernization",
+                            "amount": 23269000.0
+                        },
+                        {
+                            "id": "office-of-inspector-general",
+                            "name": "Office of Inspector General",
+                            "amount": 89147000.0
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
